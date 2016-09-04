cb425610c2125e09933e2b731d6d63eb94e0525c
Navid RahimiDanesh <naviddanesh76@gmail.com>
2016-09-04 21:21:51 +0430

    algorithm changed to bellman!

--- MCMF.cpp
+++ MCMF.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 
 using namespace std;
@@ -16,49 +15,46 @@ typedef long long C;
 
 //no need to initialize these variables!
 int V,E;
-F cap[MAXE],flow[MAXE];
+F cap[MAXE];
 C cost[MAXE],dist[MAXV],pot[MAXV];
 int to[MAXE],prv[MAXE],last[MAXV],path[MAXV];
 bool used[MAXV];
 priority_queue <pair <C, int> > q;
 
+//output
+F flow[MAXE];
+
 class MinCostFlow {
 public:
     MinCostFlow(int n);
-
-    void add_edge(int x, int y, F w, C c); // zero based && directed!
-
-    pair <F, C> search(int s, int t);
-
+    int add_edge(int x, int y, F w, C c); // zero based && directed!
     pair <F, C> mincostflow(int s, int t);
-
-    vector <pair <int, int> > flow_edges(void);
+private:
+    pair <F, C> search(int s, int t);
+    void bellman(int s);
 };
 
-vector <pair <int, int> > MinCostFlow::flow_edges(void) {
-    int x, e;
-    vector <pair <int, int> > ans;
-    REP(x,V) for(e=last[x];e>=0;e=prv[e]) if(flow[e] > 0) ans.push_back(make_pair(x,to[e]));
-    return ans;
-}
-
+///////////////////////////BACKEND!/////////////////////////
 MinCostFlow::MinCostFlow(int n){
     V = n; E = 0;
     int i; REP(i,V) last[i] = -1;
 }
-
-void MinCostFlow::add_edge(int x, int y, F w, C c){
+int MinCostFlow::add_edge(int x, int y, F w, C c){
     cap[E] = w; flow[E] = 0; cost[E] = c; to[E] = y; prv[E] = last[x]; last[x] = E; E++;
     cap[E] = 0; flow[E] = 0; cost[E] = -c; to[E] = x; prv[E] = last[y]; last[y] = E; E++;
+    return E-2;
 }
-
+void MinCostFlow::bellman(int s){
+    int i,x,e;
+    REP(i,V) pot[i] = C_INF;
+    pot[s] = 0;
+    REP(i,V+10) REP(x,V) for(e=last[x];e>=0;e=prv[e]) if(cap[e] > 0) pot[to[e]] = min(pot[to[e]], pot[x] + cost[e]);
+}
 pair <F, C> MinCostFlow::search(int s, int t){
     F ansf=0; C ansc=0;
     int i;
-
     REP(i,V) used[i] = false;
     REP(i,V) dist[i] = C_INF;
-
     dist[s] = 0; path[s] = -1; q.push(make_pair(0,s));
     while(!q.empty()){
         int x = q.top().second; q.pop();
@@ -72,22 +68,18 @@ pair <F, C> MinCostFlow::search(int s, int t){
             }
         }
     }
-
     REP(i,V) pot[i] += dist[i];
-
     if(used[t]){
         ansf = F_INF;
         for(int e=path[t];e>=0;e=path[to[e^1]]) ansf = min(ansf,cap[e]);
-        for(int e=path[t];e>=0;e=path[to[e^1]]) {ansc += cost[e] * ansf; cap[e] -= ansf; flow[e] += ansf; cap[e^1] += ansf; flow[e^1] -= ansf;}
+        for(int e=path[t];e>=0;e=path[to[e^1]]) {ansc += cost[e] * ansf; cap[e] -= ansf; cap[e^1] += ansf; flow[e] += ansf; flow[e^1] -= ansf;}
     }
-
     return make_pair(ansf,ansc);
 }
-
 pair <F, C> MinCostFlow::mincostflow(int s, int t){
     F ansf=0; C ansc=0;
     int i;
-    REP(i,V) pot[i] = 0;
+    bellman(s);
     while(1){
         pair <F, C> p = search(s,t);
         if(!used[t]) break;
@@ -95,6 +87,7 @@ pair <F, C> MinCostFlow::mincostflow(int s, int t){
     }
     return make_pair(ansf,ansc);
 }
+////////////////////////////////////////////////////
 
 int main() {
     return 0;
