cc28d264fc0b46d1f954c214de815310f975e53b
Navid RahimiDanesh <naviddanesh76@gmail.com>
2016-12-20 12:51:32 +0330

    changed convex hull code from https://github.com/jaehyunp/stanfordacm.git

--- convexhull.cpp
+++ convexhull.cpp
@@ -1,45 +1,71 @@
-//uva.onlinejudge.org : 681
-#include <complex>
-typedef long long ld;
-typedef complex <ld> point;
-#define X real()
-#define Y imag()
-#define Size(x) (int)(x).size()
+// Compute the 2D convex hull of a set of points using the monotone chain
+// algorithm.  Eliminate redundant points from the hull if REMOVE_REDUNDANT is
+// #defined.
+//
+// Running time: O(n log n)
+//
+//   INPUT:   a vector of input points, unordered.
+//   OUTPUT:  a vector of points in the convex hull, counterclockwise, starting
+//            with bottommost/leftmost point
 
-const ld EPS = 1e-12;
-vector <point> vl;//convex hull vector
-point p[N], O;//
+#include <cstdio>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+// BEGIN CUT
+#include <map>
+// END CUT
 
-ld operator ^(const point &a, const point &b) {
-	return imag(a * conj(b));
+using namespace std;
+
+#define REMOVE_REDUNDANT
+
+typedef double T;
+const T EPS = 1e-7;
+struct PT {
+  T x, y;
+  PT() {}
+  PT(T x, T y) : x(x), y(y) {}
+  bool operator<(const PT &rhs) const { return make_pair(y,x) < make_pair(rhs.y,rhs.x); }
+  bool operator==(const PT &rhs) const { return make_pair(y,x) == make_pair(rhs.y,rhs.x); }
+};
+
+T cross(PT p, PT q) { return p.x*q.y-p.y*q.x; }
+T area2(PT a, PT b, PT c) { return cross(a,b) + cross(b,c) + cross(c,a); }
+
+#ifdef REMOVE_REDUNDANT
+bool between(const PT &a, const PT &b, const PT &c) {
+  return (fabs(area2(a,b,c)) < EPS && (a.x-b.x)*(c.x-b.x) <= 0 && (a.y-b.y)*(c.y-b.y) <= 0);
 }
+#endif
 
-ld operator |(const point &a, const point &b) {
-	return real(a * conj(b));
-}
+void ConvexHull(vector<PT> &pts) {
+  sort(pts.begin(), pts.end());
+  pts.erase(unique(pts.begin(), pts.end()), pts.end());
+  vector<PT> up, dn;
+  for (int i = 0; i < pts.size(); i++) {
+    while (up.size() > 1 && area2(up[up.size()-2], up.back(), pts[i]) >= 0) up.pop_back();
+    while (dn.size() > 1 && area2(dn[dn.size()-2], dn.back(), pts[i]) <= 0) dn.pop_back();
+    up.push_back(pts[i]);
+    dn.push_back(pts[i]);
+  }
+  pts = dn;
+  for (int i = (int) up.size() - 2; i >= 1; i--) pts.push_back(up[i]);
 
-bool cmp(const point &a, const point &b) {
-	return (((a - O) ^ (b - O)) < (ld)0) || (((a - O) ^ (b - O)) == 0 && norm(a - O) < norm(b - O));
-}
-
-inline bool chk(const point &a, const point &b, const point &c) {
-	return (((b - c) ^ (a - c)) >= (ld)0);
-}
-
-void find_hull(int n) {
-	int ind = 0;
-	for(int i = 0;i < n;++i)
-		if(p[i].Y < p[ind].Y || (p[i].Y == p[ind].Y && p[i].X < p[ind].X))
-			ind = i;
-	swap(p[0], p[ind]);
-	O = p[0];
-	sort(p + 1, p + n, cmp);
-	vl.push_back(p[0]);
-	for(int i = 1;i < n;++i) {
-		while(Size(vl) > 1 && chk(p[i], vl.back(), vl[Size(vl) - 2]))
-			vl.pop_back();
-		vl.push_back(p[i]);
-	}
-	while(Size(vl) > 1 && chk(p[0], vl.back(), vl[Size(vl) - 2]))
-		vl.pop_back();
+#ifdef REMOVE_REDUNDANT
+  if (pts.size() <= 2) return;
+  dn.clear();
+  dn.push_back(pts[0]);
+  dn.push_back(pts[1]);
+  for (int i = 2; i < pts.size(); i++) {
+    if (between(dn[dn.size()-2], dn[dn.size()-1], pts[i])) dn.pop_back();
+    dn.push_back(pts[i]);
+  }
+  if (dn.size() >= 3 && between(dn.back(), dn[0], dn[1])) {
+    dn[0] = dn.back();
+    dn.pop_back();
+  }
+  pts = dn;
+#endif
 }
