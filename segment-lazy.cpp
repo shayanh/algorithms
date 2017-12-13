234009831961dbabca6b0d984e629dc951671300
Shayan Hosseini <shayan.hosseiny@gmail.com>
2017-12-13 16:27:31 +0330

    use a better example

--- segment-lazy.cpp
+++ segment-lazy.cpp
@@ -1,47 +1,47 @@
-int min_val[4 * MAXN], rgt_min[4 * MAXN], add[4 * MAXN];
+int seg[4 * MAXN], add[4 * MAXN];
 
-inline void shift(int x) {
+inline void shift(int x, int s, int e) {
 	int lc = x + x + 0, rc = x + x + 1;
-	if (add[x]) {
-		min_val[lc] += add[x];
-		min_val[rc] += add[x];
-		add[lc] += add[x];
-		add[rc] += add[x];
-	}
+
+	int mid = (s + e) / 2;
+	int l1 = mid - s, l2 = e - mid;
+
+	seg[lc] += l1 * add[x];
+	seg[rc] += l2 * add[x];
+	add[lc] += add[x];
+	add[rc] += add[x];
+	
 	add[x] = 0;
 }
 
 // lo, hi -> [)
 // s = 0, e = n, x = 1
 
-void update2(int lo, int hi, int s, int e, int x, int delta) {
+void update(int lo, int hi, int s, int e, int x, int delta) {
 	if (lo == s && hi == e) {
-		min_val[x] += delta;
-		add[x] += delta;
+		int len = (e - s);
+		seg[x] = seg[x] + len * delta;
+		add[x] = add[x] + delta;
 		return ;
 	}
-	shift(x);
+	shift(x, s, e);
 
 	int mid = (s + e) / 2;
-	if (lo < mid) update2(lo, min(mid, hi), s, mid, x + x + 0, delta);
-	if (hi > mid) update2(max(lo, mid), hi, mid, e, x + x + 1, delta);
-
 	int lc = x + x + 0, rc = x + x + 1;
-	min_val[x] = min(min_val[lc], min_val[rc]);
-	if (min_val[rc] <= min_val[lc]) rgt_min[x] = rgt_min[rc] + mid - s;
-	else rgt_min[x] = rgt_min[lc];
+	if (lo < mid) update(lo, min(mid, hi), s, mid, lc, delta);
+	if (hi > mid) update(max(lo, mid), hi, mid, e, rc, delta);
+
+	seg[x] = seg[lc] + seg[rc];
 }
 
-pii get2(int lo, int hi, int s, int e, int x) {
-	if (lo == s && hi == e) return mp(min_val[x], rgt_min[x]);
-	shift(x);
+int get(int lo, int hi, int s, int e, int x) {
+	if (lo == s && hi == e) return seg[x];
+	shift(x, s, e);
 
 	int mid = (s + e) / 2;
-	pii tmp1 = mp(INF, -1), tmp2 = mp(INF, -1);
-	if (lo < mid) tmp1 = get2(lo, min(mid, hi), s, mid, x + x + 0);
-	if (hi > mid) tmp2 = get2(max(lo, mid), hi, mid, e, x + x + 1);
+	int res = 0;
+	if (lo < mid) res += get(lo, min(mid, hi), s, mid, x + x + 0);
+	if (hi > mid) res += get(max(lo, mid), hi, mid, e, x + x + 1);
 
-	if (tmp2.X <= tmp1.X) return mp(tmp2.X, tmp2.Y + (lo < mid ? mid - lo : 0));
-	else return mp(tmp1.X, tmp1.Y);
+	return res;
 }
-
